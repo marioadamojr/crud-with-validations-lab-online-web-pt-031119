464c77e4e2d02ecc6dbf236d1fc466a08a321798
Mario Adamo Jr <marioadamojr@gmail.com>
2019-08-11 18:11:37 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,6 +2,16 @@ class Song < ApplicationRecord
   validates :title, presence: true
   validate :release_year_exists
   validate :future_release
+  validates :title, uniqueness: { scope: [:release_year, :artist_name],
+    message: "An Artist Cannot Release More Than One Song of the Same Title in the Same Year"
+  }
+
+
+  :same_title_same_year
+
+  def same_title_same_year
+
+  end
 
   def release_year_exists
     if self.released == true && !self.release_year
