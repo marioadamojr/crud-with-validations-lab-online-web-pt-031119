b2aa1e641d338d2c533505359dbac03a38d0d46c
Mario Adamo Jr <marioadamojr@gmail.com>
2019-08-11 18:47:20 +0000

    Done.

---
+++ app/models/song.rb
@@ -0,0 +1,23 @@
+class Song < ApplicationRecord
+  validates :title, presence: true
+  validates :released, inclusion: { in: [true, false] }
+  validate :release_year_exists
+  validate :future_release
+  validates :title, uniqueness: { scope: [:release_year, :artist_name],
+    message: "An Artist Cannot Release More Than One Song of the Same Title in the Same Year"
+  }
+
+  def release_year_exists
+    if self.released == true && !self.release_year
+      errors.add(:release_year, "Release Year Required")
+    end
+  end
+
+  def future_release
+    if self.release_year
+      if self.release_year > Date.current.year
+        errors.add(:release_year, "Release Year Cannot Exceed Current Year")
+      end
+    end
+  end
+end
