307a72fe8fec27c683b252cddd8d71c0a7e1ba78
Mario Adamo Jr <marioadamojr@gmail.com>
2019-08-11 18:00:54 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,11 @@
 class Song < ApplicationRecord
   validates :title, presence: true
+  validate :release_year_exists
+
+
+  def release_year_exists
+    if self.released == true && !self.release_year
+      errors.add(:release_year, "Release Year Required")
+    end
+  end
 end
