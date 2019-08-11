eac01b4c9c735d14de32662d42a8f383d77e405f
Mario Adamo Jr <marioadamojr@gmail.com>
2019-08-11 18:03:41 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,11 +1,17 @@
 class Song < ApplicationRecord
   validates :title, presence: true
   validate :release_year_exists
-
+  validate :future_release
 
   def release_year_exists
     if self.released == true && !self.release_year
       errors.add(:release_year, "Release Year Required")
     end
+  end
+
+  def future_release
+    if self.release_year > Date.current.year
+      errors.add(:release_year, "Release Year Cannot Exceed Current Year")
+
   end
 end
